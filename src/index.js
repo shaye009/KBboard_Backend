05f663854dfa2535dfe46775acbaa58102e6b19f
Yunfei Yang <yfy668@Yunfeis-MacBook-Pro.local>
2022-09-18 08:42:47 +1000

    corrected errors in eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const app = express();
-
-app.get('/',(req, res) => {
-    res.json('hello world');
-})
-
+app.get('/', (req, res) => {
+	res.json('hello world');
+});
 app.listen(3000, () => {
-    console.log('server listen')
-})
+	console.log('server listen');
+});
